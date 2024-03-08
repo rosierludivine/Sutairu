1ad4edc9daf00b012387d2ff54521905a3d980b7
Sara Awad <sara.awad@ecole-isitech.fr>
2024-03-08 10:01:00 +0100

    connection to the database

---
+++ sutairu/backend/index.js
@@ -0,0 +1,117 @@
+// Code  for mongoose config in backend
+// Filename - backend/index.js
+ 
+// To connect with your mongoDB database
+
+const mongoose = require('mongoose');
+
+const uri = "mongodb+srv://UserDB:RUV0xqiwSQjnEX9o@sutairu.dabgfok.mongodb.net/?retryWrites=true&w=majority&appName=Sutairu";
+ 
+async function connectToDatabase() {
+    try {
+        await mongoose.connect(uri, {
+            dbName: 'sutairu',
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Connecté à la base de données');
+    } catch (error) {
+        console.error('Erreur de connexion à la base de données :', error);
+    }
+}
+
+connectToDatabase();
+// Schema for users of app
+
+const UserSchema = new mongoose.Schema({
+
+    name: {
+
+        type: String,
+
+        required: true,
+
+    },
+
+    email: {
+
+        type: String,
+
+        required: true,
+
+        unique: true,
+
+    },
+
+    date: {
+
+        type: Date,
+
+        default: Date.now,
+
+    },
+});
+
+const User = mongoose.model('users', UserSchema);
+User.createIndexes();
+ 
+// For backend and express
+
+const express = require('express');
+const app = express();
+
+const cors = require("cors");
+
+console.log("App listen at port 5000");
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, resp) => {
+ 
+
+    resp.send("App is Working");
+
+    // You can check backend is working or not by 
+
+    // entering http://loacalhost:5000
+
+     
+
+    // If you see App is working means
+
+    // backend working properly
+});
+ 
+
+app.post("/register", async (req, resp) => {
+
+    try {
+
+        const user = new User(req.body);
+
+        let result = await user.save();
+
+        result = result.toObject();
+
+        if (result) {
+
+            delete result.password;
+
+            resp.send(req.body);
+
+            console.log(result);
+
+        } else {
+
+            console.log("User already register");
+
+        }
+ 
+
+    } catch (e) {
+
+        resp.send("Something Went Wrong");
+
+    }
+});
+app.listen(5000);
--- sutairu/src/pages/Inscription.js
+++ sutairu/src/pages/Inscription.js
@@ -4,6 +4,26 @@ import { useNavigate } from "react-router-dom";
 import image2 from "../images/image2.jpg";
 import "./inscription.css";
 function Inscription() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    let result = await fetch(
+      'http://localhost:5000/register', {
+      method: "post",
+      body: JSON.stringify({ name, email }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    result = await result.json();
+    console.warn(result);
+    if (result) {
+      alert("Data saved succesfully");
+      setEmail("");
+      setName("");
+    }
+  }
   const [isModalVisible, setIsModalVisible] = useState(false);
 
   // Function to toggle the modal's visibility
@@ -31,7 +51,8 @@ function Inscription() {
         <h2>INSCRIPTION</h2>
         <div class="input-group">
           <label for="name">Nom:</label>
-          <input type="text" id="name" />
+          <input type="text" placeholder="name"
+          value={name} onChange={(e) => setName(e.target.value)} />
         </div>
         <div class="input-group">
           <label for="surname">Prénom :</label>
@@ -39,7 +60,8 @@ function Inscription() {
         </div>
         <div class="input-group">
           <label for="email">Email :</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" 
+          value={email} onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div class="input-group">
           <label for="password">Mot de passe :</label>
