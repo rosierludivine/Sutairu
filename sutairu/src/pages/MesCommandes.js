6f964c0cee670170d07444ff52356c65c3e8db48
rosierludivine <91051230+rosierludivine@users.noreply.github.com>
2024-07-23 17:01:02 +0200

    add 3D in MesCommandes

--- sutairu/src/pages/MesCommandes.js
+++ sutairu/src/pages/MesCommandes.js
@@ -1,73 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "./mesdesign.css";
-import pull from "../images/pull.jpg";
-import CardItemCommandes from "../components/CardItemCommandes";
+import axios from 'axios';
+import "./mesCommandes.css";
 import { countries } from "countries-list";
+import ThreeArticle from '../components/ThreeArticle';
 
 function MesCommandes() {
-  const CardItemsCommmande = [
-    {
-      id: 1,
-      name: "Product 1",
-      size: "M",
-      price: 25,
-      image: pull,
-    },
-    {
-      id: 2,
-      name: "Product 2",
-      size: "L",
-      price: 30,
-      image: pull,
-    },
-    {
-      id: 3,
-      name: "Product 3",
-      size: "S",
-      price: 20,
-      image: pull,
-    },
-    {
-      id: 4,
-      name: "Product 4",
-      size: "XL",
-      price: 35,
-      image: pull,
-    },
-    {
-      id: 5,
-      name: "Product 4",
-      size: "XL",
-      price: 35,
-      image: pull,
-    },
-    {
-      id: 6,
-      name: "Product 4",
-      size: "XL",
-      price: 35,
-      image: pull,
-    },
-  ];
+  const [panier, setPanier] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchPanier = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/panier/lulu@test.com');
+        setPanier(response.data.articles);
+      } catch (error) {
+        console.error('Erreur lors de la récupération du panier :', error);
+      }
+    };
+
+    fetchPanier();
+  }, []);
+
+  const commander = () => {
+    navigate('/commandes');
+  };
+
+  const calculerPrixTotal = () => {
+    let total = 0;
+    panier.forEach((item) => {
+      if (item.article) {
+        total += item.article.prix * item.quantite;
+      }
+    });
+    return total;
+  };
 
   const [isModalVisible, setIsModalVisible] = useState(false);
 
-  // Function to toggle the modal's visibility
   const showModal = () => {
     setIsModalVisible(true);
   };
 
-  // Function to hide the modal
   const suivreMonColis = () => {
-    navigate("/colis"); // Change the URL to '/login'
+    navigate("/colis");
   };
-
-  const navigate = useNavigate(); // Hook to access navigate function
-
-//   const closeModal = () => {
-//     setIsModalVisible(false);
-//   };
 
   const countryOptions = Object.keys(countries).map((code) => ({
     code,
@@ -75,67 +52,90 @@ function MesCommandes() {
   }));
 
   return (
-    <div className="container">
-      <div className="articles">
-        <h1>Les Articles</h1>
-        <div className="cartsPart" style={{width: "100%"}}>
-          {CardItemsCommmande.map((item) => (
-            <CardItemCommandes key={item.id} item={item} />
-          ))}
-        </div>
-        <button type="submit" class="pay-button">
-          Total: 132€
-        </button>
-      </div>
-      {/* second part */}
-      <div class="payment-form child">
-        <h1>Le payement</h1>
-        <div class="payment-options">
-          <button class="option selected">Cartes</button>
-          <button class="option">Apple Pay</button>
-          <button class="option">PayPal</button>
-        </div>
-        <form>
-          <div class="form-group">
-            <label for="card-number" class="card-text">Numéro de Carte :</label>
-            <input type="text" id="card-number" class="placeholder" placeholder="Votre nom" />
-          </div>
-          <div class="form-group">
-            <label for="expiration" class="card-text">Expiration :</label>
-            <input type="text" id="expiration" class="placeholder" placeholder="Votre expiration" />
-          </div>
-          <div class="form-group">
-            <label for="cvc" class="card-text">CVC : </label>
-            <input type="text" id="cvc"  class="placeholder"placeholder="Votre CVC" />
-          </div>
-          <div class="form-group">
-            <label for="country" class="card-text">Pays :</label>
-            <select id="country" class="country">
-              <option value="">Sélectionnez un pays</option>
-              {countryOptions.map((country) => (
-                <option key={country.code} value={country.code}>
-                  {country.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <button type="button"  className="butt-paye" onClick={showModal}>
-            Payer
-          </button>
-          {isModalVisible && (
-            <div>
-              <div className="modal-overlay"></div>
-              <div className="modal-content">
-                <h2>Votre paiement a bien été accepté</h2>
-                <div className="modal-buttons">
-                  <button onClick={suivreMonColis} className="modal-btn">
-                    Suivre mon colis
-                  </button>
+    <div className="main-container">
+      <div className="card-container">
+        <div className="card">
+          {panier.map((item) => (
+            <div key={item.article._id} className="CardItem">
+              <div className="leftSide">
+                {item.article && (
+                  <>
+                    <ThreeArticle type={item.article.hoodie} color={item.article.couleur} />
+                    <p className="productName">{item.article.hoodie}</p>
+                  </>
+                )}
+              </div>
+              <div className="rightSide">
+                <div className='detail'>
+                  {item.article && (
+                    <>
+                      <p>Taille : {item.article.taille}</p>
+                      <p>Quantité : {item.quantite}</p>
+                      <p>Prix : {item.article.prix}</p>
+                    </>
+                  )}
                 </div>
               </div>
             </div>
-          )}
-        </form>
+          ))}
+        </div>
+        <div className="total-price-container">
+          <button className="prixBtn">
+            Prix total : ${calculerPrixTotal()}
+          </button>
+        </div>
+      </div>
+      
+      <div className="payment-container">
+        <div className="payment-form">
+          <h1>Le payement</h1>
+          <div className="payment-options">
+            <button className="option selected">Cartes</button>
+            <button className="option">Apple Pay</button>
+            <button className="option">PayPal</button>
+          </div>
+          <form>
+            <div className="form-group">
+              <label htmlFor="card-number" className="card-text">Numéro de Carte :</label>
+              <input type="text" id="card-number" className="placeholder" placeholder="Votre nom" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="expiration" className="card-text">Expiration :</label>
+              <input type="text" id="expiration" className="placeholder" placeholder="Votre expiration" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="cvc" className="card-text">CVC :</label>
+              <input type="text" id="cvc" className="placeholder" placeholder="Votre CVC" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="country" className="card-text">Pays :</label>
+              <select id="country" className="country">
+                <option value="">Sélectionnez un pays</option>
+                {countryOptions.map((country) => (
+                  <option key={country.code} value={country.code}>
+                    {country.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <button type="button" className="butt-paye" onClick={showModal}>
+              Payer
+            </button>
+            {isModalVisible && (
+              <div>
+                <div className="modal-overlay"></div>
+                <div className="modal-content">
+                  <h2>Votre paiement a bien été accepté</h2>
+                  <div className="modal-buttons">
+                    <button onClick={suivreMonColis} className="modal-btn">
+                      Suivre mon colis
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
+          </form>
+        </div>
       </div>
     </div>
   );
